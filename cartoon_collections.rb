ec503fb79dbd1123b7507754a3827b852ffb7acd
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-05 23:54:40 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  array.each_with_index do |item, index|
+    puts "#{index+1} #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |item|
+    item[0] = item[0].upcase
+    item << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  array.any? do |item|
+    item.size >4
+  end
+# Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each do |item|
+    return item if array.include?(item) == true
+  end
+  nil
+
+  # the array below is here to help
 end
